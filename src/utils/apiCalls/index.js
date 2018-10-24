13687cfbe3c020c8ac08c323650c42f406884f86
Tom King <design.tomking@gmail.com>
2018-10-24 17:27:43 -0600

    Refactor fetch requests DRY

--- src/utils/apiCalls/index.js
+++ src/utils/apiCalls/index.js
@@ -1,38 +1,32 @@
-const origin = 'https://www.APP.heroku-app.com/api/vi';
+export const fetchRequest = (path = '', options = {}) => {
+  const url = 'https://sheltered-peak-41535.herokuapp.com/api/v1' + path;
+  return fetch(url, options)
+    .then(res => res.json())
+    .then(data => data)
+    .catch(error => error);
+};
 
 export const getCompanyInfo = (id) => {
   const path = `/companies/${id}`;
-  const url = origin + path;
-  return fetch(url)
-    .then(res => res.json())
-    .then(data => data)
-    .catch(console.log);
+  return fetchRequest(path);
 };
 
 export const postCompanyInfo = (details) => {
   const path = `/companies`;
-  const url = origin + path;
   const options = {
     method: 'POST',
     headers: { 'Content-Type' : 'application/json' },
     body: JSON.stringify(details)
   };
-  return fetch(url, options)
-    .then(res => res.json())
-    .then(data => data)
-    .catch(console.log);
+  return fetchRequest(path, options);
 };
 
 export const putCompanyInfo = (id, details) => {
   const path = `/companies/${id}`;
-  const url = origin + path;
   const options = {
     method: 'PUT',
     headers: { 'Content-Type' : 'application/json' },
     body: JSON.stringify(details)
   };
-  return fetch(url, options)
-    .then(res => res.json())
-    .then(data => data)
-    .catch(console.log);
+  return fetchRequest(path, options);
 };
