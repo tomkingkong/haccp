7d6e8d5a40b106b1586e27fe3d011e64a577e465
Tom King <design.tomking@gmail.com>
2018-10-24 13:52:56 -0600

    Add POST request to add company details

--- src/containers/utils/apiCalls/index.js
+++ src/containers/utils/apiCalls/index.js
@@ -8,3 +8,17 @@ export const getCompanyInfo = (id) => {
     .then(data => data)
     .catch(console.log);
 };
+
+export const postCompanyInfo = (details) => {
+  const path = `/companies`;
+  const url = origin + path;
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify(details)
+  };
+  return fetch(url, options)
+    .then(res => res.json())
+    .then(data => data)
+    .catch(console.log);
+};
