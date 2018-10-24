6ec0e3b1dd793aef8ab170129d6bb22caf7521a6
Tom King <design.tomking@gmail.com>
2018-10-24 14:55:57 -0600

    Add mocks folder with mocks data

---
+++ src/containers/utils/apiCalls/__mocks__/mockData.js
@@ -0,0 +1,8 @@
+export const companyDetails = {
+  name: 'MeatZilla',
+  address: '90 Meat Way Denver, CO 80000',
+  phone: '720-594-9999',
+  email: 'BenLucasTom@meatzilla.com'
+};
+
+export const companyId = { id: 1 };
