9f906fd0fcfa146e455415aa1820fe84b9e70b8c
Tom King <design.tomking@gmail.com>
2018-10-25 07:59:47 -0600

    Unignore mock file

---
+++ src/utils/apiCalls/__mocks__/mockData.js
@@ -0,0 +1,16 @@
+export const companyDetails = `{
+  name: 'MeatZilla',
+  address: '90 Meat Way Denver, CO 80000',
+  phone: '720-594-9999',
+  email: 'BenLucasTom@meatzilla.com'
+}`;
+
+export const companyId = { id: 1 };
+
+export const companyDetailsWithId = `{
+  id: 1,
+  name: 'BurgerKing',
+  address: 'All The Way Denver, CO 80000',
+  phone: '720-594-9999',
+  email: 'BenLucasTom@meatzilla.com'
+}`;
