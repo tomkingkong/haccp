436805ad7921876957f74a3711c7749a77d22ce5
Tom King <design.tomking@gmail.com>
2018-10-25 07:04:06 -0600

    Add post request to add product to db

--- src/utils/apiCalls/index.js
+++ src/utils/apiCalls/index.js
@@ -30,3 +30,17 @@ export const putCompanyInfo = (id, details) => {
   };
   return fetchRequest(path, options);
 };
+
+export const postProduct = (id, details) => {
+  const path = `/products`;
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify({
+      ...details,
+      company_id: id
+    })    
+  };
+  return fetchRequest(path, options);
+};
+
