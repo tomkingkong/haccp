a33f963ff30db11d6eb7185a4783afe921798af3
Tom King <design.tomking@gmail.com>
2018-10-25 21:59:23 -0600

    Setup summary container and add test suite

---
+++ src/containers/Summary/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { } from 'prop-types';
+
+export class Summary extends Component {
+  constructor() {
+    super();
+  }
+
+	render() {
+	  return (
+	    <div>
+	      <h2>Summary</h2>
+	      <button>
+					Print
+	      </button>
+	    </div>
+	  );
+	}	
+}
+
+Summary.propTypes = {
+
+};
+
+const mapStateToProps = state => ({
+  
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Summary);
