39fce0dfc29ffde60aa5d192c66c79eee454dec0
Tom King <design.tomking@gmail.com>
2018-10-25 21:51:01 -0600

    Add ingredients form component and include name input
    Add ingredients component test suite

---
+++ src/components/IngredientsForm/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { IngredientsForm } from '.';
+
+describe('IngredientsForm Component', () => {
+  it('should match snapshot', () => {
+    const wrapper = shallow(<IngredientsForm />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
