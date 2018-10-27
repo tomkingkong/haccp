770d4993def6f818bafbe60b8474ee836da652e1
Tom King <design.tomking@gmail.com>
2018-10-27 14:39:57 -0600

    Dashboard renders products including add product button

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { object } from 'prop-types';
 
+import './Dashboard.css';
 import ProductContainer from '../ProductContainer';
+
 export class Dashboard extends Component {
+	constructor() {
+		super();
+		this.state = {
+			userProducts: [
+				{name: 'Meat Sticks', id: 1},
+				{name: 'Canned Ham', id: 1},
+				{name: 'SnickleFritz', id: 1}
+			]
+		};
+	}
 
 	render() {
 		const { history } = this.props;
+		const { userProducts } = this.state;
 	  return (
 	    <div>
 	      <h2>Dashboard</h2>
-				<section className="Products">
-				<ProductContainer history={history}/>
-				</section>
+				<div>
+					<h4 className="products-title">Products</h4>
+					<ProductContainer history={history} userProducts={userProducts}/>
+				</div>
 	    </div>
 	  );
 	}	
