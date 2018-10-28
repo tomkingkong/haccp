750ee6f257d4c3c71ba6f7600b226d5859ca5ed2
Tom King <design.tomking@gmail.com>
2018-10-28 13:29:09 -0600

    Import and export reducers through index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,21 @@
 import { combineReducers } from 'redux';
 
 import { editProduct } from './editProduct';
+import { products } from './products';
+import { ingredients } from './ingredients';
+import { receiving } from './receiving';
+import { inventory } from './inventory';
+import { processing } from './processing';
+import { packaging } from './packaging';
+import { companyInfo } from './companyInfo';
 
 export const rootReducer = combineReducers({
-  editProduct
+  editProduct,
+  companyInfo,
+  products,
+  ingredients,
+  receiving,
+  inventory,
+  processing,
+  packaging
 });
