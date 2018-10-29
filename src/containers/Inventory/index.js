ca74a1e8a726cd9204023277c707b448ce80269f
Tom King <design.tomking@gmail.com>
2018-10-29 08:27:45 -0600

    Repeat hazard plan across all plans

--- src/containers/Inventory/index.js
+++ src/containers/Inventory/index.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { object } from 'prop-types';
+import { object, array, func, number } from 'prop-types';
+
+import { 
+  ExpansionPanel, 
+  ExpansionPanelSummary, 
+  ExpansionPanelDetails, 
+  Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import { addInventoryPlan, updateInventoryPlan } from '../../actions';
+import { HazardPlan } from '../../components/HazardPlan';
 
 export class Inventory extends Component {
+
+	handleReceivingPlan = plan => {
+	  const { receiving, addInventoryPlan, updateInventoryPlan } = this.props;
+	  const alreadyExists = this.props.receiving.find(recPlan => {
+	    return recPlan.ingredientId === plan.ingredientId;
+	  });
+
+	  alreadyExists === undefined
+	    ? addInventoryPlan((receiving.length+1), plan)
+	    : updateInventoryPlan(alreadyExists.id, plan);
+	}
 	
 	handleNextClick = () => {
 	  //save the data
-		// do something
-		this.props.history.push('/summary');
+	  // do something
+	  this.props.history.push('/processing');
+	}
+
+	displayIngredientsAndPlans = () => {
+	  const { ingredients, editProduct } = this.props;
+	  const ingredientsToEdit = ingredients.filter(ing => ing.productId === editProduct);
+	  return ingredientsToEdit.map((ing, index) => {
+	    return (
+	      <ExpansionPanel key={index}>
+	        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+	          <Typography>{ing.name}</Typography>
+	        </ExpansionPanelSummary>
+	        <ExpansionPanelDetails>
+
+	          <HazardPlan {...ing} handleReceivingPlan={this.handleReceivingPlan}/>
+	        </ExpansionPanelDetails>
+	      </ExpansionPanel>
+	    );
+	  });
 	}
 
 	render() {
 	  return (
 	    <div>
 	      <h2>Inventory</h2>
+	      <section className="ingredients-inventory-edit">
+	        { this.displayIngredientsAndPlans() }
+	      </section>
 	      <button onClick={this.handleNextClick}>
 					Next Page
 	      </button>
@@ -23,15 +65,24 @@ export class Inventory extends Component {
 }
 
 Inventory.propTypes = {
-	history: object
+  history: object,
+  addInventoryPlan: func,
+  updateInventoryPlan: func,
+  ingredients: array,
+  receiving: array,
+  editProduct: number
+	
 };
 
-const mapStateToProps = state => ({
-  
+export const mapStateToProps = ({ ingredients, editProduct, receiving }) => ({
+  ingredients,
+  editProduct,
+  receiving
 });
 
-const mapDispatchToProps = dispatch => ({
-
+export const mapDispatchToProps = dispatch => ({
+  addInventoryPlan: (id, hazardPlan) => dispatch(addInventoryPlan(id, hazardPlan)),
+  updateInventoryPlan: (id, hazardPlan) => dispatch(updateInventoryPlan(id, hazardPlan))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Inventory);
--- src/containers/Receiving/index.js
+++ src/containers/Receiving/index.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import { object, array, func, number } from 'prop-types';
 
 import { 
-	ExpansionPanel, 
-	ExpansionPanelSummary, 
-	ExpansionPanelDetails, 
-	Typography } from '@material-ui/core';
+  ExpansionPanel, 
+  ExpansionPanelSummary, 
+  ExpansionPanelDetails, 
+  Typography } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { addReceivingPlan, updateReceivingPlan } from '../../actions';
@@ -14,48 +14,48 @@ import { HazardPlan } from '../../components/HazardPlan';
 
 export class Receiving extends Component {
 
-	handleReceivingPlan = (plan) => {
-		const { receiving, addReceivingPlan, updateReceivingPlan } = this.props;
-		const alreadyExists = this.props.receiving.find(recPlan => {
-			return recPlan.ingredientId === plan.ingredientId;
-		});
+	handleReceivingPlan = plan => {
+	  const { receiving, addReceivingPlan, updateReceivingPlan } = this.props;
+	  const alreadyExists = this.props.receiving.find(recPlan => {
+	    return recPlan.ingredientId === plan.ingredientId;
+	  });
 
-		alreadyExists === undefined
-			? addReceivingPlan((receiving.length+1), plan)
-			: updateReceivingPlan(alreadyExists.id, plan);
+	  alreadyExists === undefined
+	    ? addReceivingPlan((receiving.length+1), plan)
+	    : updateReceivingPlan(alreadyExists.id, plan);
 	}
 	
 	handleNextClick = () => {
 	  //save the data
-		// do something
-    this.props.history.push('/inventory');
+	  // do something
+	  this.props.history.push('/inventory');
 	}
 
 	displayIngredientsAndPlans = () => {
-		const { ingredients, editProduct } = this.props;
-		const ingredientsToEdit = ingredients.filter(ing => ing.productId === editProduct);
-		return ingredientsToEdit.map((ing, index) => {
-			return (
-				<ExpansionPanel key={index}>
-					<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-						<Typography>{ing.name}</Typography>
-					</ExpansionPanelSummary>
-					<ExpansionPanelDetails>
+	  const { ingredients, editProduct } = this.props;
+	  const ingredientsToEdit = ingredients.filter(ing => ing.productId === editProduct);
+	  return ingredientsToEdit.map((ing, index) => {
+	    return (
+	      <ExpansionPanel key={index}>
+	        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+	          <Typography>{ing.name}</Typography>
+	        </ExpansionPanelSummary>
+	        <ExpansionPanelDetails>
 
-						<HazardPlan {...ing} handleReceivingPlan={this.handleReceivingPlan}/>
-					</ExpansionPanelDetails>
-				</ExpansionPanel>
-			);
-		});
+	          <HazardPlan {...ing} handleReceivingPlan={this.handleReceivingPlan}/>
+	        </ExpansionPanelDetails>
+	      </ExpansionPanel>
+	    );
+	  });
 	}
 
 	render() {
 	  return (
 	    <div>
 	      <h2>Receiving</h2>
-				<section className="ingredients-receiving-edit">
-					{ this.displayIngredientsAndPlans() }
-				</section>
+	      <section className="ingredients-receiving-edit">
+	        { this.displayIngredientsAndPlans() }
+	      </section>
 	      <button onClick={this.handleNextClick}>
 					Next Page
 	      </button>
@@ -65,23 +65,23 @@ export class Receiving extends Component {
 }
 
 Receiving.propTypes = {
-	history: object,
-	addReceivingPlan: func,
-	updateReceivingPlan: func,
-	ingredients: array,
-	receiving: array,
-	editProduct: number
+  history: object,
+  addReceivingPlan: func,
+  updateReceivingPlan: func,
+  ingredients: array,
+  receiving: array,
+  editProduct: number
 };
 
-const mapStateToProps = ({ ingredients, editProduct, receiving }) => ({
-	ingredients,
-	editProduct,
-	receiving
+export const mapStateToProps = ({ ingredients, editProduct, receiving }) => ({
+  ingredients,
+  editProduct,
+  receiving
 });
 
-const mapDispatchToProps = dispatch => ({
-	addReceivingPlan: (id, hazardPlan) => dispatch(addReceivingPlan(id, hazardPlan)),
-	updateReceivingPlan: (id, hazardPlan) => dispatch(updateReceivingPlan(id, hazardPlan))
+export const mapDispatchToProps = dispatch => ({
+  addReceivingPlan: (id, hazardPlan) => dispatch(addReceivingPlan(id, hazardPlan)),
+  updateReceivingPlan: (id, hazardPlan) => dispatch(updateReceivingPlan(id, hazardPlan))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Receiving);
