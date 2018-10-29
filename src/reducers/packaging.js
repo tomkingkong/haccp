8b220ff9467c2465589d7a35ce6d441d012964c2
Tom King <design.tomking@gmail.com>
2018-10-29 08:28:19 -0600

    Update reducers for plans to include add and create

--- src/reducers/packaging.js
+++ src/reducers/packaging.js
@@ -1,8 +1,11 @@
 export const packaging = (state=[], action) => {
   switch (action.type) {
     case 'ADD_PACKAGING_PLAN': {
-      const { ingredientId } = action;
-      return [...state, { ingredientId }];
+      return [...state, { id:action.id, ...action.plan }];
+    }
+    case 'UPDATE_PACKAGING_PLAN': {
+      const newState = state.filter(plan => plan.ingredientId !== action.plan.ingredientId);
+      return [...newState, { id:action.id, ...action.plan }];
     }
     default:
       return state;
--- src/reducers/processing.js
+++ src/reducers/processing.js
@@ -1,8 +1,11 @@
 export const processing = (state=[], action) => {
   switch (action.type) {
     case 'ADD_PROCESSING_PLAN': {
-      const { ingredientId } = action;
-      return [...state, { ingredientId }];
+      return [...state, { id:action.id, ...action.plan }];
+    }
+    case 'UPDATE_PROCESSING_PLAN': {
+      const newState = state.filter(plan => plan.ingredientId !== action.plan.ingredientId);
+      return [...newState, { id:action.id, ...action.plan }];
     }
     default:
       return state;
