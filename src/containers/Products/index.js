73412c2ce0c2f165b58a83d88370732b91016653
Tom King <design.tomking@gmail.com>
2018-10-29 12:42:12 -0600

    Change route flow

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -9,60 +9,60 @@ import { postProduct } from '../../utils/apiCalls';
 import { editProduct, addProduct } from '../../actions';
 
 export class Products extends Component {
-	constructor() {
+  constructor() {
     super();
     this.state = {
       productName: ''
     };
   }
 	handleNextClick = async () => {
-		const { productName } = this.state;
-		// post new product and retreive id
-		// const id = await postProduct(companyID, productName);
+	  const { productName } = this.state;
+	  // post new product and retreive id
+	  // const id = await postProduct(companyID, productName);
 
-		// retreive product id, set to redux store
-		// add product to products array
-		this.props.addProduct(4, productName);
-		// set product id to edit
-		this.props.editProduct(4);
-		// continue to ingredients
-    this.props.history.push('/plans/ingredients');
+	  // retreive product id, set to redux store
+	  // add product to products array
+	  this.props.addProduct(4, productName);
+	  // set product id to edit
+	  this.props.editProduct(4);
+	  // continue to ingredients
+	  this.props.history.push('/plans/ingredients');
 	}
 
 	handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
+	  const { name, value } = event.target;
+	  this.setState({ [name]: value });
+	}
 
 	render() {
 	  return (
 	    <div>
 	      <h2>Products</h2>
-				<form >
-					<TextField
-						id="standard-dense"
-						name="productName"
-						value={this.state.productName}
-						onChange={this.handleChange}
-						label="Product name"
-						required />
-					<Button 
-						variant="contained" 
-						color="primary" 
-						required onClick={this.handleNextClick}>
+	      <form >
+	        <TextField
+	          id="standard-dense"
+	          name="productName"
+	          value={this.state.productName}
+	          onChange={this.handleChange}
+	          label="Product name"
+	          required />
+	        <Button 
+	          variant="contained" 
+	          color="primary" 
+	          required onClick={this.handleNextClick}>
 						Save & Continue
-						<SendIcon style={{marginLeft:'0.5rem'}}/>
-					</Button>
-				</form>
+	          <SendIcon style={{marginLeft:'0.5rem'}}/>
+	        </Button>
+	      </form>
 	    </div>
 	  );
 	}	
 }
 
 Products.propTypes = {
-	history: object,
-	editProduct: func,
-	addProduct: func
+  history: object,
+  editProduct: func,
+  addProduct: func
 };
 
 const mapStateToProps = state => ({
@@ -70,8 +70,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-	editProduct: id => dispatch(editProduct(id)),
-	addProduct: (id, name) => dispatch(addProduct(id, name))
+  editProduct: id => dispatch(editProduct(id)),
+  addProduct: (id, name) => dispatch(addProduct(id, name))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
