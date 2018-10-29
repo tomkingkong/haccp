2f2128d4b77c76a54cc3d59612969fd32e757849
Tom King <design.tomking@gmail.com>
2018-10-28 19:26:32 -0600

    User can add new ingredients to form
    Saves to redux store

--- src/components/IngredientsForm/index.js
+++ src/components/IngredientsForm/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { } from 'prop-types';
+import { func } from 'prop-types';
+
+import { Button, TextField } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+
+import './IngredientsForm.css';
 
 export class IngredientsForm extends Component {
   constructor() {
     super();
     this.state = {
-      ingredientName: ''
+      ingredientName: '',
+      ingredients: []
     };
+  }
+
+  handleSave = event => {
+    event.preventDefault();
+    // this.props.handleNextClick(this.state.ingredients);
   }
 
   handleChange = event => {
@@ -14,19 +25,46 @@ export class IngredientsForm extends Component {
     this.setState({ [name]: value });
   }
 
+  handleNewIngredient = () => {
+    const { ingredientName, ingredients } = this.state;
+    const newIngredient = { name: ingredientName };
+    this.props.handleNewIngredient(ingredientName);
+    ingredients.push(newIngredient);
+    this.setState({ ingredientName: ''});
+  }
+
+  displayIngredients = () => {
+    return this.state.ingredients.map((ing, index) => {
+      return <p key={index}>{ing.name}</p>;
+    });
+  }
+
   render() {
     return (
-      <form>
-        <input 
-          type="text"
-          name="ingredientName"
-          value={this.state.ingredientName}
-          onChange={this.handleChange}
-          placeholder="Add ingredient name" 
-          required />
-      </form>
+      <div>
+        <section className="ingredients">
+        { this.displayIngredients() }
+        </section>
+        <form className="form">
+          <TextField
+            id="standard-dense"
+            name="ingredientName"
+            value={this.state.ingredientName}
+            onChange={this.handleChange}
+            label="Ingredient name"
+            required />
+          <Button variant="contained" onClick={this.handleNewIngredient}>
+            <AddIcon style={{marginRight: '0.2rem', color:'forestgreen'}}/>
+            Add Ingredient
+          </Button>
+        </form>
+      </div>
     );
   }
 }
+
+IngredientsForm.propTypes = {
+  handleNewIngredient: func
+};
 
 export default IngredientsForm;
