2c17c2ee92b06898fc51548b3dc50d948274c92c
Tom King <design.tomking@gmail.com>
2018-10-28 19:29:13 -0600

    User can add product to redux store
    Sets current product to edit

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { object } from 'prop-types';
+import { object, func } from 'prop-types';
+
+import { Button, TextField } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+
+import { postProduct } from '../../utils/apiCalls';
+import { editProduct, addProduct } from '../../actions';
 
 export class Products extends Component {
-	
-	handleNextClick = () => {
-	  //save the data
-		// do something
-    this.props.history.push('/ingredients');
+	constructor() {
+    super();
+    this.state = {
+      productName: ''
+    };
+  }
+	handleNextClick = async () => {
+		const { productName } = this.state;
+		// post new product and retreive id
+		// const id = await postProduct(companyID, productName);
 
+		// retreive product id, set to redux store
+		// add product to products array
+		this.props.addProduct(4, productName);
+		// set product id to edit
+		this.props.editProduct(4);
+		// continue to ingredients
+    this.props.history.push('/ingredients');
 	}
+
+	handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
 
 	render() {
 	  return (
 	    <div>
 	      <h2>Products</h2>
-	      <button onClick={this.handleNextClick}>
-					Next Page
-	      </button>
+				<form >
+					<TextField
+						id="standard-dense"
+						name="productName"
+						value={this.state.productName}
+						onChange={this.handleChange}
+						label="Product name"
+						required />
+					<Button 
+						variant="contained" 
+						color="primary" 
+						required onClick={this.handleNextClick}>
+						Save & Continue
+						<SendIcon style={{marginLeft:'0.5rem'}}/>
+					</Button>
+				</form>
 	    </div>
 	  );
 	}	
 }
 
 Products.propTypes = {
-	history: object
+	history: object,
+	editProduct: func,
+	addProduct: func
 };
 
 const mapStateToProps = state => ({
@@ -32,7 +70,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-
+	editProduct: id => dispatch(editProduct(id)),
+	addProduct: (id, name) => dispatch(addProduct(id, name))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
