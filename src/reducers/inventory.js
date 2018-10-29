f27a72c4a010f1ea1c7079a8af270fd139deb881
Tom King <design.tomking@gmail.com>
2018-10-29 08:28:19 -0600

    Update reducers for plans to include add and create

--- src/reducers/inventory.js
+++ src/reducers/inventory.js
@@ -1,8 +1,11 @@
 export const inventory = (state=[], action) => {
   switch (action.type) {
     case 'ADD_INVENTORY_PLAN': {
-      const { ingredientId } = action;
-      return [...state, { ingredientId }];
+      return [...state, { id:action.id, ...action.plan }];
+    }
+    case 'UPDATE_INVENTORY_PLAN': {
+      const newState = state.filter(plan => plan.ingredientId !== action.plan.ingredientId);
+      return [...newState, { id:action.id, ...action.plan }];
     }
     default:
       return state;
