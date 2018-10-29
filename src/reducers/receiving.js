e0e1f3afafd25dd3d575be1edc1673dce385b96c
Tom King <design.tomking@gmail.com>
2018-10-28 19:30:55 -0600

    Add update receiving plan to receiving reducer

--- src/reducers/receiving.js
+++ src/reducers/receiving.js
@@ -1,8 +1,11 @@
 export const receiving = (state=[], action) => {
   switch (action.type) {
     case 'ADD_RECEIVING_PLAN': {
-      const { ingredientId } = action;
-      return [...state, { ingredientId }];
+      return [...state, { id:action.id, ...action.plan }];
+    }
+    case 'UPDATE_RECEIVING_PLAN': {
+      const newState = state.filter(plan => plan.ingredientId !== action.plan.ingredientId);
+      return [...newState, { id:action.id, ...action.plan }];
     }
     default:
       return state;
