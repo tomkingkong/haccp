fed4507edc06d9b7d7167bee03716d7da382b831
benjaminpaige <benjamin.paige@gmail.com>
2018-10-31 13:08:00 -0600

    Add summary rows component

--- src/containers/Summary/index.js
+++ src/containers/Summary/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { object } from 'prop-types';
 
+import {SummaryRow} from '../SummaryRow';
 import './index.css';
 
 export class Summary extends Component {
 	constructor() {
-		super()
+		super();
 		this.state = {
-			ingredient:
+			ingredients: [
     {
       name: 'beef things',
       receiving_from: 'supplier name',
@@ -41,6 +42,38 @@ export class Summary extends Component {
       packaging_hazard_phys: false,
       packaging_hazard_handling: false
 		 },
+		 {
+      name: 'beef things',
+      receiving_from: 'supplier name',
+      receiving_letter: true,
+      receiving_dna: false,
+      receiving_other: 'some other text',
+      receiving_hazard_bio: true,
+      receiving_hazard_chem: true,
+      receiving_hazard_phys: false,
+      receiving_hazard_handling: 'something',
+      storage_type: 'cold',
+      storage_dna: false,
+      storage_other: 'something else',
+      storage_hazard_bio: false,
+      storage_hazard_chem: true,
+      storage_hazard_phys: false,
+      storage_hazard_handling: 'handled this way',
+      preparation_method: 'mixing it up',
+      preparation_dna: false,
+      preparation_other: 'some other toher string',
+      preparation_hazard_bio: false,
+      preparation_hazard_chem: false,
+      preparation_hazard_phys: true,
+      preparation_hazard_handling: 'heres a thing',
+      packaging_method: 'i haz a bucket',
+      packaging_dna: true,
+      packaging_other: '',
+      packaging_hazard_bio: false,
+      packaging_hazard_chem: false,
+      packaging_hazard_phys: false,
+      packaging_hazard_handling: false
+		 }],
 		 company:
       {
         name: 'foodco llc',
@@ -57,8 +90,15 @@ export class Summary extends Component {
       }
 		}
 	}
+
+	makeSummaryRows = () => {
+		return this.state.ingredients.map(ingredient => {
+			return <SummaryRow ingredient={ingredient}/>
+		})
+	}
+
 	render() {
-		const {ingredient, company, product} = this.state;
+		const {ingredients, company, product} = this.state;
 	  return (
 	    <div className="summary-container">
 				<div className="summary-info-header">
@@ -67,7 +107,6 @@ export class Summary extends Component {
 					<h2 className="summary-company-name">{company.name}</h2>
 					<h2 className="summary-product-name">{product.name}</h2>
 				</div>
-				
 				<div className="summary-product-header">
 					<p>{company.team_member_1_name}</p>
 					<p>{company.team_member_1_title}</p>
@@ -84,262 +123,9 @@ export class Summary extends Component {
 						<th>Processing</th>
 						<th>Packaging</th>
 					</tr>
-					<tr>
-						<td>{ingredient.name}</td>
-						
-						<td>
-	
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									 {ingredient.receiving_dna ? 'Does not apply' : ''}
-								</p>
-							</div>
-							
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-								Letter of Auth on file:
-								</p >
-									<p className="summary-info-single-line">{ingredient.receiving_letter ? 'Yes' : 'No'}
-									</p>
-							</div>
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Bio Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.receiving_hazard_bio ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Chem Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.receiving_hazard_chem ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Physical Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.receiving_hazard_phys ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Hazard Handling:
-								</p>
-								<p className="summary-table-item">
-								{ingredient.receiving_hazard_handling}
-								</p>
-							</div>
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Received From:
-								</p>
-								<p className="summary-table-item">
-									{ingredient.receiving_from}
-								</p>
-							</div>
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Other: 
-								</p>
-								<p className="summary-table-item">
-									{ingredient.receiving_other}
-								</p>
-							</div>
-						</td> 
-						
-						<td>
-
-						<div className="summary-bool">
-								<p className="summary-info-single-line">
-									 {ingredient.storage_dna ? 'Does not apply' : ''}
-								</p>
-							</div>
-							
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Bio Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.storage_hazard_bio ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Chem Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.storage_hazard_chem ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Physical Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.storage_hazard_phys ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Hazard Handling:
-								</p>
-								<p className="summary-table-item">
-								{ingredient.storage_hazard_handling}
-								</p>
-							</div>
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Received From:
-								</p>
-								<p className="summary-table-item">
-									{ingredient.receiving_from}
-								</p>
-							</div>
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Other: 
-								</p>
-								<p className="summary-table-item">
-									{ingredient.storage_other}
-								</p>
-							</div>
-						</td>
-						
-						<td>
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									 {ingredient.preparation_dna ? 'Does not apply' : ''}
-								</p>
-							</div>
-							
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Bio Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.preparation_hazard_bio ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Chem Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.preparation_hazard_chem ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Physical Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.preparation_hazard_phys ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Hazard Handling:
-								</p>
-								<p className="summary-table-item">
-								{ingredient.preparation_hazard_handling}
-								</p>
-							</div>
-							
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Other: 
-								</p>
-								<p className="summary-table-item">
-									{ingredient.preparation_other}
-								</p>
-							</div>
-							
-							</td> 
-						
-						<td>
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									 {ingredient.packaging_dna ? 'Does not apply' : ''}
-								</p>
-							</div>
-							
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Bio Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.packaging_hazard_bio ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Chem Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.packaging_hazard_chem ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-bool">
-								<p className="summary-info-single-line">
-									Physical Hazard:  
-								</p>
-								<p className="summary-info-single-line"> 
-								{ingredient.packaging_hazard_phys ? 'yes' : 'no'}
-								</p>
-							</div>
-
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Hazard Handling:
-								</p>
-								<p className="summary-table-item">
-								{ingredient.packaging_hazard_handling}
-								</p>
-							</div>
-
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Packaging method:
-								</p>
-								<p className="summary-table-item">
-								{ingredient.packaging_method}
-								</p>
-							</div>
-							
-							<div className="summary-info-dual-line">
-								<p className="summary-table-item summary-table-item-header">
-									Other: 
-								</p>
-								<p className="summary-table-item">
-									{ingredient.packaging_other}
-								</p>
-							</div>
-
-						</td>
-					</tr>
+					
+						{this.makeSummaryRows()}
 				</table>
-
-
-
-
-
-
 	      <button onClick={() => this.props.history.push('/dashboard')}>
 					Print
 	      </button>
