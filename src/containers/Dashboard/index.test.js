e2f92926c74b407326c50b232c760634dcfec5b5
Tom King <design.tomking@gmail.com>
2018-10-30 20:33:23 -0600

    Add state change tests to dashboard component
    Update snaps

--- src/containers/Dashboard/index.test.js
+++ src/containers/Dashboard/index.test.js
@@ -12,4 +12,20 @@ describe('Dashboard', () => {
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should have default userProducts state', () => {
+    const expected = [];
+    expect(wrapper.state().userProducts).toEqual(expected);
+  });
+
+  it('should userProducts state on mount', () => {
+    const mockData = [
+      { name: 'Meat Sticks', id: 1 },
+      { name: 'Canned Ham', id: 2 },
+      { name: 'SnickleFritz', id: 3 }
+    ];
+    wrapper = shallow(<Dashboard products={mockData} />);
+    wrapper.instance().componentDidMount();
+    expect(wrapper.state().userProducts).toEqual(mockData);
+  });
 });
