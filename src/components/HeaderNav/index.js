b98cac4fac987ac3a9d9a588b0488db8b7c51e77
Tom King <design.tomking@gmail.com>
2018-10-31 09:56:07 -0600

    Add header nav login and signup buttons

--- src/components/HeaderNav/index.js
+++ src/components/HeaderNav/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { object } from 'prop-types';
 
+import './index.css';
+
 export const HeaderNav = ({ history }) => (
 	<nav className="header-nav">
-		<h1 className="header-text" onClick={() => history.push('/')}>HACCP Planner</h1>
+		<h1 className="header-text" 
+			onClick={() => history.push('/')}>
+			HACCP Planner</h1>
+		<div className="nav-btns">
+			<button 
+				className="signup-login" 
+				onClick={() => history.push('/login')}>
+				Log In</button>
+			<button 
+				className="signup-login" 
+				onClick={() => history.push('/signup')}>
+				Sign Up</button>
+		</div>
 	</nav>
 );
 
