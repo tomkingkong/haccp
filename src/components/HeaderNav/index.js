f553e930ea9928cb667e724ce0e2d4b4fff73f4d
Tom King <design.tomking@gmail.com>
2018-10-31 10:08:26 -0600

    Add conditional logic to display log out button

--- src/components/HeaderNav/index.js
+++ src/components/HeaderNav/index.js
@@ -3,23 +3,44 @@ import { object } from 'prop-types';
 
 import './index.css';
 
-export const HeaderNav = ({ history }) => (
-	<nav className="header-nav">
-		<h1 className="header-text" 
-			onClick={() => history.push('/')}>
-			HACCP Planner</h1>
-		<div className="nav-btns">
-			<button 
-				className="signup-login" 
+export const HeaderNav = ({ history }) => {
+	const path = history.location.pathname;
+	const shouldLogInOrSignUp = ( 
+		path === '/' 
+		|| path === '/login'
+		|| path === '/signup' );
+
+	const signInLogIn = () => (
+		<React.Fragment>
+			<button className="signup-login" 
 				onClick={() => history.push('/login')}>
-				Log In</button>
-			<button 
-				className="signup-login" 
+				Log In
+			</button>
+			<button className="signup-login" 
 				onClick={() => history.push('/signup')}>
-				Sign Up</button>
-		</div>
-	</nav>
-);
+				Sign Up
+			</button>
+		</React.Fragment>
+	)
+
+	const logOut = () => (
+		<button className="signup-login" 
+			onClick={() => history.replace('/')}>
+			Log Out
+		</button>
+	)
+
+	return (
+		<nav className="header-nav">
+			<h1 className="header-text" 
+				onClick={() => history.push('/')}>
+				HACCP Planner</h1>
+			<div className="nav-btns">
+				{ shouldLogInOrSignUp ? signInLogIn() : logOut() }
+			</div>
+		</nav>
+	)
+};
 
 HeaderNav.propTypes = {
 	history: object
