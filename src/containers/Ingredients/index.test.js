f5aadaecc26b562163091451fcb6c21930c88e28
Tom King <design.tomking@gmail.com>
2018-10-30 21:02:58 -0600

    Add handleNextClick test to ingredients container

--- src/containers/Ingredients/index.test.js
+++ src/containers/Ingredients/index.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { Ingredients } from '.';
+import { Ingredients, mapDispatchToProps, mapStateToProps } from '.';
+import { addIngredient } from '../../actions';
 
 describe('Ingredients', () => {
   let wrapper;
+  let mockFn;
   beforeEach(() => {
-    wrapper = shallow(<Ingredients ingredients={[]} addIngredient={() => jest.fn()}/>);
+    mockFn = jest.fn();
+    wrapper = shallow(<Ingredients ingredients={[]} addIngredient={mockFn}/>);
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('handleNextClick', () => {
+    it('should call history to push to next page', () => {
+      const mockHistory = {
+        push: jest.fn()
+      }
+      wrapper = shallow(<Ingredients 
+        ingredients={[]}
+        history={mockHistory} />);
+      wrapper.instance().handleNextClick();
+      expect(mockHistory.push).toHaveBeenCalled();
+    });
+  });
+
 });
