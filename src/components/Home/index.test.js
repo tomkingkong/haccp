532c018461cfa13939a22880f8cf955974afd699
Tom King <design.tomking@gmail.com>
2018-10-31 21:59:20 -0600

    Test home component paths

--- src/components/Home/index.test.js
+++ src/components/Home/index.test.js
@@ -8,4 +8,13 @@ describe('Home Component', () => {
     const wrapper = shallow(<Home />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should send user to signup on get started click', () => {
+    const mockFn = jest.fn();
+    const history = { push: mockFn };
+    const wrapper = shallow(<Home history={history} />);
+    wrapper.find('.get-started').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+    expect(mockFn).toHaveBeenCalledWith('/signup');
+  });
 });
