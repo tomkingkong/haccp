e8e6928fcfc21b79511d5b13417922d0aa53d55c
Tom King <design.tomking@gmail.com>
2018-10-31 22:00:22 -0600

    Test companyinfo component props and submission calls

--- src/containers/CompanyInfo/index.test.js
+++ src/containers/CompanyInfo/index.test.js
@@ -17,7 +17,6 @@ describe('CompanyInfo', () => {
 		const expected = {
 			id: '',
       name: '', 
-      email: '',
       address: '',
       phone: '',
       team_member_1_name: '',
@@ -30,7 +29,6 @@ describe('CompanyInfo', () => {
     const mockData = {
       id: 1,
       name: 'MeatFactory', 
-      email: 'meatFactory@meat.com',
       address: '34 address st',
       phone: '934103451',
       team_member_1_name: 'Lucah',
@@ -41,8 +39,23 @@ describe('CompanyInfo', () => {
     expect(wrapper.state()).toEqual(mockData);
   });
 
-  describe('handleSubmit', () => {
+  describe('handleSubmit', async () => {
+    const mockFn = jest.fn();
+		const mockHistory = {
+			push: mockFn
+    };
+    const mockParse = jest.fn();
+		const wrapper = shallow(<CompanyInfo 
+			history={mockHistory} 
+      setCompanyInfo={mockParse} />);
 
+		const mockEvent = {
+			preventDefault: jest.fn()
+		};
+		await wrapper.instance().handleSubmit(mockEvent);
+		expect(mockParse).toHaveBeenCalled();
+		expect(mockFn).toHaveBeenCalled();
+		expect(mockFn).toHaveBeenCalledWith('/dashboard');
   });
 
   describe('handleChange', () => {
@@ -57,7 +70,6 @@ describe('CompanyInfo', () => {
       expect(wrapper.state().name).toEqual('Schmeckles');
     });
   });
-
 
   describe('mapStateToProps', () => {
     it('should be provided with a products array from props', () => {
@@ -95,6 +107,16 @@ describe('CompanyInfo', () => {
       };
       const results = mapStateToProps(mockState);
       expect(results).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with setCompanyInfo when setCompanyInfo is called', () => {
+      const mockDispatch = jest.fn();
+      const mockSetCompanyInfo = { info: {name:"pickles"}, type: 'SET_COMPANY_USER' };
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.setCompanyInfo({name: "pickles"});
+      expect(mockDispatch).toHaveBeenCalledWith(mockSetCompanyInfo);
     });
   });
 });
